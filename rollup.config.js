5abd1cc08939d09daf073132e7adf3e8fad02f3c
BinaryBand <g-maf.backpedal641@passinbox.com>
2025-06-18 19:54:51 -0700

    Added compiled types

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,27 @@
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: { file: 'dist/tezos-wallet.js', format: 'cjs' },
-  plugins: [
-    commonjs(),
-    json(),
-    resolve({ preferBuiltins: true }),
-    terser({ format: { comments: false } }),
-    typescript(),
-    typescriptPaths(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: { file: 'dist/index.js', format: 'cjs' },
+    plugins: [
+      commonjs(),
+      json(),
+      nodeResolve({ preferBuiltins: true }),
+      terser({ format: { comments: false } }),
+      typescript(),
+      typescriptPaths(),
+    ],
+  },
+  {
+    input: './src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), typescript(), typescriptPaths()],
+  },
+];
